a77a2b4dfaba22b811d2082f71ca23569ccffca3
harshvardhan-security <70275362+harshvardhan-security@users.noreply.github.com>
2026-03-06 19:13:45 +0000

    Deploying to gh-pages from @ harshvardhan-security/harshvardhan-security.github.io@585b2c084c48ba4aae4d59b9d8d502e13cca5655 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-projects",
+          title: "Projects",
+          description: "AI Security Projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-blog",
           title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "CV",
